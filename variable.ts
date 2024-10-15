72eefb02ce9b99295735a17a4fd743a0de755a03
dev.Dibendu <dibendughosh12@gmail.com>
2024-10-15 13:09:36 +0530

    added some more code

--- variable.ts
+++ variable.ts
@@ -49,3 +49,113 @@
 
 // var result = addNum("Samrat",100);
 // console.log(result);
+
+// function names(x1:string,x2:string):string{
+//     return x1+` `+x2;
+// }
+
+// var result = names("Samrat","Ghosh");
+// console.log(result);
+
+//!SECTION Optional Parameters
+
+// function test(x1:number,x2?:number):void{
+//     console.log(x1);
+//     console.log(x2);
+// }
+
+// test(10);
+
+//!SECTION Default Parameters
+
+// function test(x1:number,x2=100):void{
+//     console.log(x1);
+//     console.log(x2);
+// }
+
+// test(10,350);
+
+//!SECTION for loop
+
+// function loop (n:number[]):void{
+//     for(var i=0;i<n.length;i++){
+//         console.log(n[i]);
+//     }
+// }
+
+// var x1:number[] = [10,42,25,6];
+// console.log(loop(x1));
+
+//!SECTION REST Parameters
+
+// function test(...n:number[]):void{
+//     for(var i=0;i<n.length;i++){
+//         console.log(n[i]);
+//     }
+// }
+
+// test(10,20,30,40,50,60,70,80,90,100);
+
+// function test(...n:any[]):void{
+//     for(var i:number=0;i<n.length;i++){
+//         console.log(n[i]);
+//     }
+// }
+
+// test("samrat",10,40,50,"Ghosh",52);
+
+// function test(i:string,...n:number[]):void{
+//     for(var x:number=0;x<n.length;x++){
+//         console.log(n[x]);
+//     }
+//     console.log(i);
+// }
+
+// test("Samrat",10,40,50,80,90,35,25,30);
+
+// function test(name:string,city:string):void{
+//     console.log(`${name} ${city}`);
+// }
+
+// test("Samrat","Bangalore");
+
+//!SECTION ARROW FUNCTIONS
+
+// var v1 = (x1:number,x2:number):number =>
+// {
+//     return x1+x2;
+// }
+
+// var result:number = v1(10,20);
+// console.log(result);
+
+//ANCHOR - Without RETURN
+
+// var addVal = (v1:number,v2:number):number => v1+v2;
+// var result:number = addVal(10,20);
+// console.log(result);
+
+//ANCHOR - Develope a funcion to return multiplication of two numbers
+
+// function multi(x1:number,x2:number):number{
+//     return x1*x2;
+// }
+
+// var result:number = multi(10,5);
+// console.log(result);
+
+//ANCHOR - CRESTE AN ARRAY AND STORES IT'S VALUES AND PRINT IT IN USING FOR LOOPS
+
+// var arr:number[] = [10,20,30,40,50,60];
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+//ANCHOR - CREATE AN ARROW FUNCTIONS TO MULTIPLE TWO NUMBERS
+
+var val = (v1:number,v2:number)=>{
+    return v1*v2;
+}
+
+var result = val(10,20);
+console.log(result);
