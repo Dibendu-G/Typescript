1f89ad619510d93e5fbdc432a9478567a8c42d75
dev.Dibendu <dibendughosh12@gmail.com>
2024-10-15 12:36:13 +0530

    added variables and Functions

---
+++ variable.js
@@ -0,0 +1,36 @@
+//ANCHOR - Variables
+// var x1:number = 10;
+// console.log(x1);
+// var x2:number = 104.5;
+// console.log(x2);
+// var x3:boolean = true;
+// console.log(x3);
+// var x4:boolean = false;
+// console.log(x4);
+// var x5:string = "Samrat";
+// console.log(x5);
+// var x6:any =1450.54699;
+// console.log(x6);
+//SECTION - Creating An Array
+// var arr:number[] = [10,20,45,35,85];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+//SECTION Creating an Function
+// function addnum(x1:number, x2:number):number
+// {
+//     return x1 + x2;
+// }
+// var result = addnum(10,20);
+// console.log(result);
+// function test():void{
+//     console.log("Testing");
+// }
+// test();
+function addNum(x1, x2) {
+    return x1 + x2;
+}
+var result = addNum("Samrat", 100);
+console.log(result);
